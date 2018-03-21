1417d7ff2fb16c222a5453ed27899e50944efd81
Alvaro de Oliveira Santos <alvarooliveirasantos2010@gmail.com>
2018-03-21 07:37:33 -0300

    Correção na saida de texto

--- ValidarTexto/src/br/com/stream/impl/Main.java
+++ ValidarTexto/src/br/com/stream/impl/Main.java
@@ -7,9 +7,9 @@ public class Main {
 	        //para estar com outro texto alterar a entrada abaixo
 	    	char resul = LocalizaVogal.primeiroCaracter(new CaracterStream("aAbBABacafe"));
 	        if(resul != LocalizaVogal.VAZIO) {
-	            System.out.println("\nCaracter encontrado: " + resul);
+	            System.out.println("Caracter encontrado: " + resul);
 	        } else {
-	            System.out.println("\nCaracter nao localizado.");
+	            System.out.println("Caracter nao localizado.");
 	        }
 	    }
 	}
